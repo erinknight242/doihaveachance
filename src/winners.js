07264c51c63d36b54b931f1fd83e3605b6addcad
erinknight242 <erinknight242@gmail.com>
2021-03-19 23:53:32 -0500

    Editable js for winners

--- src/winners.js
+++ src/winners.js
@@ -1,6 +1,130 @@
+const VirginiaTech = 0;
+const Florida = 1;
+const Colgate = 2;
+const Arkansas = 3;
+const Drexel = 4;
+const Illinois = 5;
+const UtahState = 6;
+const TexasTech = 7;
+const OralRoberts = 8;
+const OhioSt = 9;
+const Hartford = 10;
+const Baylor = 11;
+const GeorgiaTech = 12;
+const LoyolaChicago = 13;
+const OregonState = 14;
+const Tennessee = 15;
+const Liberty = 16;
+const OklahomaState = 17;
+const Wisconsin = 18;
+const NorthCarolina = 19;
+const ClevelandState = 20;
+const Houston = 21;
+const NorthTexas = 22;
+const Purdue = 23;
+const Rutgers = 24;
+const Clemson = 25;
+const Syracuse = 26;
+const SanDiegoState = 27;
+const MoreheadState = 28;
+const WestVirginia = 29;
+const Winthrop = 30;
+const Villanova = 31;
+const Georgetown = 32;
+const Colorado = 33;
+const UNCGreensboro = 34;
+const FloridaState = 35;
+const EasternWashington = 36;
+const Kansas = 37;
+const StBonaventure = 38;
+const LSU = 39;
+const TexasSouthern = 40;
+const Michigan = 41;
+const UCSantaBarbara = 42;
+const Creighton = 43;
+const Iona = 44;
+const Alabama = 45;
+const Drake = 46;
+const USC = 47;
+const GrandCanyon = 48;
+const Iowa = 49;
+const Maryland = 50;
+const UConn = 51;
+const Ohio = 52;
+const Virginia = 53;
+const Missouri = 54;
+const Oklahoma = 55;
+const NorfolkState = 56;
+const Gonzaga = 57;
+const UCLA = 58;
+const BYU = 59;
+const AbelineChristian = 60;
+const Texas = 61;
+const VCU = 62;
+const Oregon = 63;
+
 window.winners = {
-    round64winner1: { team: 1, played: true},
-    round64winner2: { team: 3, played: true},
-    round64winner3: { team: 5, played: true},
-    round64winner4: { team: 7, played: true},
+    round64winner1: { team: Florida, played: true},
+    round64winner2: { team: Arkansas, played: true},
+    round64winner3: { team: Illinois, played: true},
+    round64winner4: { team: TexasTech, played: true},
+    round64winner5: { team: OralRoberts, played: true },
+    round64winner6: { team: Baylor, played: true },
+    round64winner7: { team: LoyolaChicago, played: true },
+    round64winner8: { team: OregonState, played: true },
+    round64winner9: { team: OklahomaState, played: true },
+    round64winner10: { team: Wisconsin, played: true },
+    round64winner11: { team: null, played: false },
+    round64winner12: { team: null, played: false },
+    round64winner13: { team: null, played: false },
+    round64winner14: { team: null, played: false },
+    round64winner15: { team: null, played: false },
+    round64winner16: { team: null, played: false },
+    round64winner17: { team: null, played: false },
+    round64winner18: { team: null, played: false },
+    round64winner19: { team: null, played: false },
+    round64winner20: { team: null, played: false },
+    round64winner21: { team: null, played: false },
+    round64winner22: { team: null, played: false },
+    round64winner23: { team: null, played: false },
+    round64winner24: { team: null, played: false },
+    round64winner25: { team: null, played: false },
+    round64winner26: { team: null, played: false },
+    round64winner27: { team: null, played: false },
+    round64winner28: { team: null, played: false },
+    round64winner29: { team: null, played: false },
+    round64winner30: { team: null, played: false },
+    round64winner31: { team: null, played: false },
+    round64winner32: { team: null, played: false },
+    round32winner33: { team: null, played: false },
+    round32winner34: { team: null, played: false },
+    round32winner35: { team: null, played: false },
+    round32winner36: { team: null, played: false },
+    round32winner37: { team: null, played: false },
+    round32winner38: { team: null, played: false },
+    round32winner39: { team: null, played: false },
+    round32winner40: { team: null, played: false },
+    round32winner41: { team: null, played: false },
+    round32winner42: { team: null, played: false },
+    round32winner43: { team: null, played: false },
+    round32winner44: { team: null, played: false },
+    round32winner45: { team: null, played: false },
+    round32winner46: { team: null, played: false },
+    round32winner47: { team: null, played: false },
+    round32winner48: { team: null, played: false },
+    sweet16winner49: { team: null, played: false },
+    sweet16winner50: { team: null, played: false },
+    sweet16winner51: { team: null, played: false },
+    sweet16winner52: { team: null, played: false },
+    sweet16winner53: { team: null, played: false },
+    sweet16winner54: { team: null, played: false },
+    sweet16winner55: { team: null, played: false },
+    sweet16winner56: { team: null, played: false },
+    elite8winner57: { team: null, played: false },
+    elite8winner58: { team: null, played: false },
+    elite8winner59: { team: null, played: false },
+    elite8winner60: { team: null, played: false },
+    final4winner61: { team: null, played: false },
+    final4winner62: { team: null, played: false },
+    championshipwinner63: { team: null, played: false }
 };
